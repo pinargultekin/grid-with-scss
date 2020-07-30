1d16dc6759d4901a843626a712574a9491fbca2a
Pinar <pinargultekin@yahoo.com>
2020-07-30 13:20:19 -0400

    content and initial scss created

--- src/index.js
+++ src/index.js
@@ -5,7 +5,36 @@ import "./styles.scss";
 
 const App = () => (
   <div>
-    <h1>Hello</h1>
+      <section className="grid-test">
+        <div className="row">
+          <div className="1-2">Column 1 of 2 </div>
+          <div className="1-2">Column 1 of 2  </div>
+        </div>
+        <div className="row">
+          <div className="1-3">Column 1 of 3  </div>
+          <div className="1-3">Column 1 of 3  </div>
+          <div className="1-3">Column 1 of 3  </div>
+        </div>
+        <div className="row">
+          <div className="1-3">Column 1 of 3  </div>
+          <div className="2-3">Column 2 of 3  </div>
+        </div>
+        <div className="row">
+          <div className="1-4">Column 1 of 4  </div>
+          <div className="1-4">Column 1 of 4  </div>
+          <div className="1-4">Column 1 of 4  </div>
+          <div className="1-4">Column 1 of 4  </div>
+        </div>
+        <div className="row">
+          <div className="1-4">Column 1 of 4  </div>
+          <div className="1-4">Column 1 of 4  </div>
+          <div className="2-4">Column 2 of 4  </div>
+        </div>
+        <div className="row">
+          <div className="1-4">Column 1 of 4  </div>
+          <div className="3-4">Column 3 of 4  </div>
+        </div>
+      </section>
   </div>
 );
 
