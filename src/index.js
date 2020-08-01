a469eef75e2a4329e6836d90a6abb12c086c64ef
Pinar <pinargultekin@yahoo.com>
2020-08-01 12:33:05 -0400

    style added

--- src/index.js
+++ src/index.js
@@ -7,32 +7,32 @@ const App = () => (
   <div>
       <section className="grid-test">
         <div className="row">
-          <div className="1-2">Column 1 of 2 </div>
-          <div className="1-2">Column 1 of 2  </div>
+          <div className="col-1-2">Column 1 of 2 </div>
+          <div className="col-1-2">Column 1 of 2  </div>
         </div>
         <div className="row">
-          <div className="1-3">Column 1 of 3  </div>
-          <div className="1-3">Column 1 of 3  </div>
-          <div className="1-3">Column 1 of 3  </div>
+          <div className="col-1-3">Column 1 of 3  </div>
+          <div className="col-1-3">Column 1 of 3  </div>
+          <div className="col-1-3">Column 1 of 3  </div>
         </div>
         <div className="row">
-          <div className="1-3">Column 1 of 3  </div>
-          <div className="2-3">Column 2 of 3  </div>
+          <div className="col-1-3">Column 1 of 3  </div>
+          <div className="col-2-3">Column 2 of 3  </div>
         </div>
         <div className="row">
-          <div className="1-4">Column 1 of 4  </div>
-          <div className="1-4">Column 1 of 4  </div>
-          <div className="1-4">Column 1 of 4  </div>
-          <div className="1-4">Column 1 of 4  </div>
+          <div className="col-1-4">Column 1 of 4  </div>
+          <div className="col-1-4">Column 1 of 4  </div>
+          <div className="col-1-4">Column 1 of 4  </div>
+          <div className="col-1-4">Column 1 of 4  </div>
         </div>
         <div className="row">
-          <div className="1-4">Column 1 of 4  </div>
-          <div className="1-4">Column 1 of 4  </div>
-          <div className="2-4">Column 2 of 4  </div>
+          <div className="col-1-4">Column 1 of 4  </div>
+          <div className="col-1-4">Column 1 of 4  </div>
+          <div className="col-2-4">Column 2 of 4  </div>
         </div>
         <div className="row">
-          <div className="1-4">Column 1 of 4  </div>
-          <div className="3-4">Column 3 of 4  </div>
+          <div className="col-1-4">Column 1 of 4  </div>
+          <div className="col-3-4">Column 3 of 4  </div>
         </div>
       </section>
   </div>
